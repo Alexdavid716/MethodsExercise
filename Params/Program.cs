0b74ad4c16956128d85969b1bea37ea5b6bfd9e5
Alex David <alexdavid716@gmail.com>
2024-03-04 08:08:54 -0500

    params

---
+++ Params/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+        Console.WriteLine(Sum(2, 4));               
+        Console.WriteLine(Sum(2, 4, 6));            
+        Console.WriteLine(Sum(1, 1, 1, 1, 1));      
+        Console.WriteLine(Multiply(10, 2));         
+    }
+
+   
+    static int Sum(params int[] numbers)
+    {
+        int result = 0;
+        foreach (int num in numbers)
+        {
+            result += num;
+        }
+        return result;
+    }
+
+
+    static int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+}
